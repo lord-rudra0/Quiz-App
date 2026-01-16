b0e4133ce8268c4a1d2e7e284fbe79d1473ddc04
Rudra Pratap Singh <singh.pratap.rudra.dev@gmail.com>
2026-01-16 21:02:20 +0530

    Add version probe

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -24,4 +24,9 @@ app.get('/', (req, res) => {
     res.send('Quiz App Backend is running');
 });
 
+// Deployment Probe
+app.get('/api/version', (req, res) => {
+    res.json({ version: '1.0.1', features: ['download'] });
+});
+
 module.exports = app;
